2f2cbde6dfa20bf4f20220c207dd8749e63e87ac
Felipe Knorr Kuhn <git@knorrium.info>
2012-05-09 17:51:20 -0300

    First public commit

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 google-books-downloader
 =======================
 
-Just a proof of concept Google Books downloader
+Just a proof of concept Google Books downloader.
+
+This script started as a pet project to learn about CasperJS, but since it might be helpful to other people, I have decided to release it.
+
+Google Books is a great source of information, but I would love to have an offline version of some books to read later on my iPad.
+
+I wanted to scrape the HTML and extract all the image information that contains the actual pages, but Google Books implements lazy loading to download the pages as you scroll down on the page.
+
+Thus, the only way to extract the images is using an actual, javascript enabled browser. Or a a headless implementation of one, and this is where PhantomJS comes in :)
+
+I have put together some very ugly code that loads a book on Google Books, scrolls all the way down and prints the URLs for the files so you can download them all with wget.
+
+In the future, I would like to generate a PDF with all the pages together. Wanna help me out? :)
+
+Dependencies
+------------
+
+- CapserJS 0.6.6
+- PhantomJS 1.5
+- wget
+
+Known Limitations
+-----------------
+
+- The book URL is hardcoded
+- The script does not actually download books, but rather prints a list of wget commands you must run (due to a download bug in CasperJS), I suggest redirecting the output to a .sh or .bat file
+- It does not download books with limited preview (though I have plans to add proxy and Tor support to bypass the preview limitation :)
+
+Usage
+-----
+
+`casperjs > pages.sh
+sh pages.sh`
+
+TODO
+----
+
+`[ ] Load the URL from the command line
+[ ] Change the filenames to make it easier to combine or read all the files
+[ ] Merge the files to PDF`
---
+++ gbd.js
@@ -0,0 +1,66 @@
+var pages = [];
+
+//TODO: load the URL from the command line
+var url = "http://books.google.com.br/books?id=AS4DAAAAMBAJ&printsec=frontcover&hl=pt-BR&source=gbs_ge_summary_r&cad=0#v=onepage&q&f=false";
+
+Array.prototype.exists = function(search){
+  for (var i=0; i<this.length; i++)
+    if (this[i] == search) return true;
+        
+  return false;
+}
+
+var casper = require('casper').create({
+    onError: function(self, m) {
+        console.log('FATAL:' + m);
+        self.exit();
+    },
+    viewportSize: {
+        width: 1024,
+        height: 600
+    }
+});
+
+casper.start(url, function() {
+    var loading_div = "div[style*='position: absolute; left: 0px; color: rgb(128, 128, 128); font-size: 13px; background-color: white; bottom: 0px; -webkit-user-select: none;']";
+    var page_down = ".SPRITE_page_down";
+    casper.then(function(){
+
+        this.waitFor(
+                function() {
+                    this.click(page_down);
+                    var elem = this.evaluate(function(){
+                        var viewport_div = "div[id='viewport'] > div > div > div";
+                        var elem = document.querySelector(viewport_div).offsetParent;
+                        return elem;
+                    });
+                    var current_position = elem.scrollHeight - elem.scrollTop - elem.offsetHeight;
+                    return (elem.scrollHeight - elem.scrollTop - elem.offsetHeight === 0);
+            }, 
+                function() {
+                    // this.echo("Done scrolling");
+                },
+                    function(){
+                        // this.echo("Timeout!?");
+                    },
+                        Infinity //This is a strong candidate to win a TheDailyWTF award :)
+            );
+    });
+});
+
+casper.on('resource.received', function(resource) {
+    if ((resource.url.indexOf("&pg=P") != -1) && (resource.url.indexOf("&jscmd=click3") == -1)){
+        if (!pages.exists(resource.url)){
+            pages.push(resource.url);
+            var url = resource.url;
+            var file = (url.substring(url.indexOf("&pg=")+4, url.indexOf("&img"))) + ".png";
+            this.echo("wget --quiet --output-document='" + file + "' --user-agent=\"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.3) Gecko/2008092416 Firefox/3.0.3\" '" + resource.url + "'");
+            //TODO: Implement this when CasperJS issue #73 is fixed: https://github.com/n1k0/casperjs/issues/73
+            // try { 
+            //     casper.download(resource.url, file);
+            // }
+        }
+    }
+});
+
+casper.run();
