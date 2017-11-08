3b427e30b9e51da923c788b18967c73694c10984
Geremia <Geremia@users.noreply.github.com>
2017-11-08 13:43:40 -0700

    improved PNG filenames' numbering does not re-download already-existing PNGs

--- gbd.js
+++ gbd.js
@@ -4,11 +4,12 @@ var casper = require('casper').create({
     logLevel: "debug"
 });
 var x = require('casper').selectXPath;
+var fs = require('fs');
 
 //Get the Google Books ID from the command line:
 casper.cli.drop("cli");
 casper.cli.drop("casper-path");
-if (casper.cli.args.length === 0 && Object.keys(casper.cli.options).length === 0) {
+if (casper.cli.args.length === 0) {
     casper.echo("Please pass the Google Books ID (e.g., zAsjkHJ8aP8C) to the command line.").exit();
 }
 var url = 'https://books.google.com/books?id=' + casper.cli.args[0] + '&printsec=frontcover#v=onepage&q&f=false';
@@ -20,8 +21,14 @@ casper.on('resource.received', function(resource) {
     if (URL.indexOf("content?") !== -1 &&
         URL.indexOf("&pg=") !== -1 &&
         URL.indexOf("&img=1") !== -1) { //if it's an image URL
-        var file = URL.substring(URL.indexOf("&pg=") + 4, URL.indexOf("&img")) + ".png";
-        if (pngFileNames.indexOf(file) === -1) {
+
+        //form file name with 4-digit numbers padded with zero:
+        var pgPrefix = URL.substring(URL.indexOf("&pg=P") + 5, URL.indexOf("&pg=P") + 6);
+        var pgNum = URL.substring(URL.indexOf("&pg=P") + 6, URL.indexOf("&img"));
+        var pgNumPadded = String("000" + pgNum).slice(-4);
+        var file = pgPrefix + pgNumPadded + ".png";
+
+        if (pngFileNames.indexOf(file) === -1 && !fs.exists(file)) {
             try {
                 this.echo(file);
                 casper.download(URL, file);
@@ -30,9 +37,10 @@ casper.on('resource.received', function(resource) {
                 this.echo(e);
             }
         }
-    } else if (URL.indexOf("&jscmd=click3") !== -1) { //if the page is unavailable
+    } else if (URL.indexOf("&jscmd=click3") !== -1) { //advance to next page
         this.click(x('//*[@src="/googlebooks/images/kennedy/page_right.png"]'));
     }
 });
 
 casper.run();
+
